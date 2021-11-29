2c1f640d3ee3db85f8dfac841a6678347a265878
Shourya Gupta <65845457+shourya-gupta@users.noreply.github.com>
2021-11-29 19:19:43 +0530

    Add files via upload

---
+++ Code/iot_try.ino
@@ -0,0 +1,62 @@
+#define BLYNK_PRINT Serial
+#include <OneWire.h>
+#include <SPI.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS D2
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+char auth[] =”yeQ2H9_LKVLAGVSQzk3aLA6J75L25uKd”;
+char ssid[] = “Airtel_9810156712”;
+char pass[] = “air22503”;
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
+void sendSensor()
+{
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+
+if (isnan(h) || isnan(t)) {
+Serial.println(“Failed to read from DHT sensor!”);
+return;
+}
+
+Blynk.virtualWrite(V5, h); //V5 is for Humidity
+Blynk.virtualWrite(V6, t); //V6 is for Temperature
+}
+void setup()
+{
+Serial.begin(9600);
+dht.begin();
+
+timer.setInterval(1000L, sendSensor);
+Blynk.begin(auth, ssid, pass);
+sensors.begin();
+}
+int sensor=0;
+int output=0;
+void sendTemps()
+{
+sensor=analogRead(A0);
+output=(145-map(sensor,0,1023,0,100)); //in place 145 there is 100(it change with the change in sensor)
+delay(1000);
+sensors.requestTemperatures();
+float temp = sensors.getTempCByIndex(0);
+Serial.println(temp);
+Serial.print(“moisture = “);
+Serial.print(output);
+Serial.println(“%”);
+Blynk.virtualWrite(V1, temp);
+Blynk.virtualWrite(V2,output);
+delay(1000);
+}
+void loop()
+{
+Blynk.run();
+timer.run();
+sendTemps();
+}
